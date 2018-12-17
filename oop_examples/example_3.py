a1b3a77e331e3984732535e7eaf6f64b930780a5
Gökhan Gerdan <gokhang1327@gmail.com>
2018-12-18 00:58:51 +0300

    Update example_3.py

--- oop_examples/example_3.py
+++ oop_examples/example_3.py
@@ -7,18 +7,3 @@ class Dog:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
-
-"""
->>> class Dog:
-...     pass
-...
->>> Dog()
-<__main__.Dog object at 0x1004ccc50>
->>> Dog()
-<__main__.Dog object at 0x1004ccc90>
->>> a = Dog()
->>> b = Dog()
->>> a == b
-False
-"""
