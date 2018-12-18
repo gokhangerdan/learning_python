12a2b581ba6ee49a26e9b7881135d619ffcbbda3
Gökhan Gerdan <gokhang1327@gmail.com>
2018-12-18 15:59:31 +0300

    Create example_7.py

---
+++ oop_examples/example_7.py
@@ -0,0 +1,54 @@
+# Parent class
+class Dog:
+
+    # Class attribute
+    species = 'mammal'
+
+    # Initializer / Instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # instance method
+    def description(self):
+        return "{} is {} years old".format(self.name, self.age)
+
+    # instance method
+    def speak(self, sound):
+        return "{} says {}".format(self.name, sound)
+
+
+# Child class (inherits from Dog() class)
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return "{} runs {}".format(self.name, speed)
+
+
+# Child class (inherits from Dog() class)
+class Bulldog(Dog):
+    def run(self, speed):
+        return "{} runs {}".format(self.name, speed)
+
+
+# Child classes inherit attributes and
+# behaviors from the parent class
+jim = Bulldog("Jim", 12)
+print(jim.description())
+
+# Child classes have specific attributes
+# and behaviors as well
+print(jim.run("slowly"))
+
+# Is jim an instance of Dog()?
+print(isinstance(jim, Dog))
+
+# Is julie an instance of Dog()?
+julie = Dog("Julie", 100)
+print(isinstance(julie, Dog))
+
+# Is johnny walker an instance of Bulldog()
+johnnywalker = RussellTerrier("Johnny Walker", 4)
+print(isinstance(johnnywalker, Bulldog))
+
+# Is julie and instance of jim?
+print(isinstance(julie, jim))
