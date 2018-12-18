1d1b7422d0333e44c3f937d9901ebec492039f94
Gökhan Gerdan <gokhang1327@gmail.com>
2018-12-18 12:46:43 +0300

    Create example_4.py

---
+++ flask_restful_api_examples/example_4.py
@@ -0,0 +1,19 @@
+from flask_restful import fields, marshal_with
+
+resource_fields = {
+    'task':   fields.String,
+    'uri':    fields.Url('todo_ep')
+}
+
+class TodoDao(object):
+    def __init__(self, todo_id, task):
+        self.todo_id = todo_id
+        self.task = task
+
+        # This field will not be sent in the response
+        self.status = 'active'
+
+class Todo(Resource):
+    @marshal_with(resource_fields)
+    def get(self, **kwargs):
+        return TodoDao(todo_id='my_todo', task='Remember the milk')
